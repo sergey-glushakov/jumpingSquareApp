30838a5325f2fda9709809e4c8e2a717b6d42d51
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2020-09-12 21:41:30 +0300

    create square

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
-$start.addEventListener('click', startGame)
+
+let score = 0;
 
 function startGame() {
-  $game.style.backgroundColor = "#fff";
+  $game.style.backgroundColor = '#fff';
   $start.classList.add('hide');
   renderBox();
 }
 
-function renderBox(){
+function renderBox() {
+  $game.innerHTML = '';
+  let box = document.createElement('div');
 
+  box.style.height = box.style.width = '50px';
+  box.style.position = 'absolute';
+  box.style.backgroundColor = '#000';
+  box.style.top = '50px';
+  box.style.left = '50px';
+  box.style.cursor = 'pointer';
+  box.setAttribute('data-box', true);
+
+
+  $game.insertAdjacentElement('afterbegin', box);
 }
+
+function handleBoxClick(event) {
+  if (event.target.dataset.box) {
+    score = score + 1;
+    renderBox();
+  }
+  ;
+}
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
